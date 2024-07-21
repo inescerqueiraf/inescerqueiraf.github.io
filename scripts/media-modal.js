5884c18192057234c414dfdff3c75b4b0178cc68
Tito Griné <tgrine@singlestore.com>
2024-07-21 01:24:34 +0100

    First draft of custom scrollbar.

---
+++ scripts/media-modal.js
@@ -0,0 +1,98 @@
+const createOverlayMediaModal = (modalContent) => {
+  let overlay = document.createElement("div");
+  overlay.className = "overlay";
+
+  let modal = document.createElement("div");
+  modal.className = "overlay-media-modal";
+
+  let modalHeader = document.createElement("div");
+  modalHeader.className = "modal-header";
+
+  let modalCloseButton = document.createElement("div");
+  modalCloseButton.className = "modal-close";
+
+  let closeIconPixel1 = document.createElement("div");
+  let closeIconPixel2 = document.createElement("div");
+  let closeIconPixel3 = document.createElement("div");
+  let closeIconPixel4 = document.createElement("div");
+  let closeIconPixel5 = document.createElement("div");
+  let closeIconPixel6 = document.createElement("div");
+  let closeIconPixel7 = document.createElement("div");
+  let closeIconPixel8 = document.createElement("div");
+  let closeIconPixel9 = document.createElement("div");
+
+  closeIconPixel1.className = "close-icon-pixel";
+  closeIconPixel2.className = "close-icon-pixel";
+  closeIconPixel3.className = "close-icon-pixel";
+  closeIconPixel4.className = "close-icon-pixel";
+  closeIconPixel5.className = "close-icon-pixel";
+  closeIconPixel6.className = "close-icon-pixel";
+  closeIconPixel7.className = "close-icon-pixel";
+  closeIconPixel8.className = "close-icon-pixel";
+  closeIconPixel9.className = "close-icon-pixel";
+
+  modalCloseButton.appendChild(closeIconPixel1);
+  modalCloseButton.appendChild(closeIconPixel2);
+  modalCloseButton.appendChild(closeIconPixel3);
+  modalCloseButton.appendChild(closeIconPixel4);
+  modalCloseButton.appendChild(closeIconPixel5);
+  modalCloseButton.appendChild(closeIconPixel6);
+  modalCloseButton.appendChild(closeIconPixel7);
+  modalCloseButton.appendChild(closeIconPixel8);
+  modalCloseButton.appendChild(closeIconPixel9);
+
+  modalHeader.appendChild(modalCloseButton);
+  modal.appendChild(modalHeader);
+  modal.appendChild(modalContent);
+  overlay.appendChild(modal);
+  document.body.appendChild(overlay);
+
+  overlay.addEventListener("click", function () {
+    document.body.removeChild(overlay);
+  });
+
+  modal.addEventListener("click", function (event) {
+    event.stopPropagation();
+  });
+
+  modalCloseButton.addEventListener("click", function () {
+    document.body.removeChild(overlay);
+  });
+};
+
+const addClickableMediaModal = () => {
+  clickableImageMedia = document.querySelectorAll("img.clickable-media");
+
+  clickableImageMedia.forEach(function (media) {
+    media.addEventListener("click", function () {
+      let modalContent = document.createElement("img");
+      modalContent.src = this.src.split("?")[0];
+
+      createOverlayMediaModal(modalContent);
+    });
+  });
+
+  clickableVideoMedia = document.querySelectorAll("video.clickable-media");
+
+  clickableVideoMedia.forEach(function (media) {
+    media.addEventListener("click", function () {
+      let modalContent = document.createElement("video");
+      modalContent.controls = true;
+      modalContent.autoplay = true;
+
+      console.log(this.children);
+
+      let source = document.createElement("source");
+      source.src = this.firstElementChild.src;
+      source.type = this.firstElementChild.type;
+
+      modalContent.appendChild(source);
+
+      createOverlayMediaModal(modalContent);
+    });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  addClickableMediaModal();
+});
